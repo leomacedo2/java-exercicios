a98618ce3dbebc2457b28415339912f254204655
Leo Macedo <leomacedo19@gmail.com>
2025-12-18 05:11:15 -0300

    feat: Exercicio 11 parte 3. Validação de entrada

--- src/Ex11_POO/Ex11_POO.java
+++ src/Ex11_POO/Ex11_POO.java
@@ -9,15 +9,36 @@ public class Ex11_POO {
 
         // Criando uma instância da classe Pessoa
         Pessoa pessoa1 = new Pessoa("Leo", 34);
+        Pessoa pessoa2 = new Pessoa("Ana", -5); // Testando idade negativa
+
 
         // Exibindo os dados da pessoa
         pessoa1.exibirDados();
+        System.out.println();
+        pessoa2.exibirDados();
+        System.out.println();
 
         // Fazendo aniversário
         pessoa1.fazerAniversario();
+        System.out.println();
 
         // Exibindo os dados novamente
         pessoa1.exibirDados();
+        System.out.println();
+
+        System.out.println("Nome da pessoa 1: " + pessoa1.getNome());
+        System.out.println("Idade da pessoa 1: " + pessoa1.getIdade());
+        System.out.println("Nome da pessoa 2: " + pessoa2.getNome());
+        System.out.println("Idade da pessoa 2: " + pessoa2.getIdade());
+        System.out.println();
+
+        // Pessoa 2 faz aniversário
+        pessoa2.fazerAniversario();
+        System.out.println();
+
+        // Apresentando as pessoas
+        System.out.println(pessoa1.apresentar());
+        System.out.println(pessoa2.apresentar());
     }
 }
 
--- src/Ex11_POO/Pessoa.java
+++ src/Ex11_POO/Pessoa.java
@@ -11,6 +11,9 @@ public class Pessoa {
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
+        if (idade < 0) {
+            this.idade = 0; // Garantir que a idade não seja negativa
+        }
     }
 
     // Método para exibir os dados da pessoa
@@ -23,6 +26,11 @@ public class Pessoa {
     public void fazerAniversario() {
         idade++;
         System.out.println("Feliz aniversário, " + nome + "! Agora você tem " + idade + " anos.");
+    }
+
+    // Método para apresentar a pessoa
+    public String apresentar() {
+        return "Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.";
     }
 
     // Getters
