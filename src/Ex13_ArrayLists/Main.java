2314088de4166237e579513aad050b93cfc55dae
Leo Macedo <leomacedo19@gmail.com>
2026-01-03 17:52:47 -0300

    feat: preparação ambiente para exercicio 13 de Arraylists

---
+++ src/Ex13_ArrayLists/Main.java
@@ -0,0 +1,25 @@
+package Ex13_ArrayLists;
+
+public class Main {
+    public static void main(String[] args) {
+        Motor motorFraco = new Motor(100);
+        Motor motorForte = new Motor(300);
+
+        Carro carroPopular = new Carro("Carro Popular", motorFraco);
+        Carro carroEsportivo = new Carro("Carro Esportivo", motorForte);
+
+
+        carroPopular.ligar();
+        carroEsportivo.ligar();
+
+        carroPopular.exibirFichaTecnica();
+
+        carroPopular.trocarMotor(motorForte);
+
+        carroPopular.ligar();
+
+        carroPopular.exibirFichaTecnica();
+    }
+
+} 
+
